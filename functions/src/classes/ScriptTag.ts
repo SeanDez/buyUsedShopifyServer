024f7b371dc42adde538a9c8cd804b00395c21ac
Sean Dez <sd@seandezoysa.com>
2019-12-11 18:18:50 +0700

    convert test class to extend, allowing access to protected methods

--- functions/src/classes/ScriptTag.ts
+++ functions/src/classes/ScriptTag.ts
@@ -25,7 +25,7 @@ interface GetAllScriptTags {
 
 class ScriptTag extends ShopifyApiBase {
   protected filePath: string;
-  public localFileName: string;
+  protected localFileName: string;
   
   
   constructor(storeDomain: string, generalToken: string, filePath: string) {
@@ -57,7 +57,7 @@ class ScriptTag extends ShopifyApiBase {
     }
   }
   
-  public getFileName(filePathOrUrl: string): string {
+  protected getFileName(filePathOrUrl: string): string {
     const fileNameMatch: string[]|null = filePathOrUrl.match(/\/?(\w+.js)$/);
     if (Boolean(fileNameMatch)) {
       // capture group is always assigned to [1], even when full match === capture group
--- functions/src/classes/__tests__/ScriptTag.test.ts
+++ functions/src/classes/__tests__/ScriptTag.test.ts
@@ -1,9 +1,10 @@
 import ScriptTag from "../ScriptTag";
 
-// beforeEach(() => {
-//   jest.spyOn(ScriptTag.prototype, 'getFileName').mockImplementation(() => 'scriptTag.js');
-// });
-//
+class ScriptTagTester extends ScriptTag {}
+
+beforeEach(() => {
+});
+
 afterEach(() => {
   // bad naming. This means clear all mocks
   jest.restoreAllMocks();
@@ -13,35 +14,38 @@ describe('constructor', () => {
   test("localFileName: stub test", () => {
     // modifies the class itself through the prototype
     jest
-      .spyOn(ScriptTag.prototype, "getFileName")
+      .spyOn(ScriptTagTester.prototype, "getFileName")
       .mockImplementation(() => "scriptTag.js");
     
-    const scriptTag: ScriptTag = new ScriptTag("subdomain.shopify.com", 'generalTokenValue', "../path/to/file.js");
+    
+    const scriptTagTester: ScriptTagTester = new ScriptTagTester("subdomain.shopify.com", 'generalTokenValue', "../path/to/file.js");
     
     
     // expect(scriptTag.getFileName('x')).toEqual('scriptTag.js');
-    expect(scriptTag.localFileName).toEqual('scriptTag.js');
+    expect(scriptTagTester.localFileName).toEqual('scriptTag.js');
     
   });
   
   test('localFileName: mockReturnValueOnce', () => {
-    jest.spyOn(ScriptTag.prototype, "getFileName")
+    jest.spyOn(ScriptTagTester.prototype, "getFileName")
       .mockReturnValueOnce("29vk.js");
-    const scriptTag2 = new ScriptTag('x', 'x', '8.js');
-    expect(scriptTag2.localFileName).toEqual('29vk.js');
+    const scriptTagTester = new ScriptTagTester('x', 'x', '8.js');
+    expect(scriptTagTester.localFileName).toEqual('29vk.js');
   });
   
   test('localFileName: single number filename input with no preceding folder slashes', () => {
-    const scriptTag2 = new ScriptTag('x', 'x', '8.js');
-    expect(scriptTag2.localFileName).toEqual('8.js');
+    const scriptTagTester = new ScriptTagTester('x', 'x', '8.js');
+    expect(scriptTagTester.localFileName).toEqual('8.js');
   });
   
   test('localFileName blank', () => {
-    jest.spyOn(ScriptTag.prototype, "getFileName")
+    jest.spyOn(ScriptTagTester.prototype, "getFileName")
       .mockImplementationOnce(() => "dummy constructor call");
-    const scriptTag = new ScriptTag("", "", "");
+    const scriptTagTester = new ScriptTagTester("", "", "");
+    
+    // invocation must be done inside a callback to catch the throw
     expect(() => {
-      scriptTag.getFileName("")
+      scriptTagTester.getFileName("")
     }).toThrow();
   })
 });
