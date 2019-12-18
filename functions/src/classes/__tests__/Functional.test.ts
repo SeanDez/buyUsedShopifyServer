d2ec3305255c566ccabfe291958fdeb8504f8f51
Sean Dez <sd@seandezoysa.com>
2019-12-18 13:36:51 +0700

    setup npm test scripts correctly

---
+++ functions/src/classes/__tests__/Functional.test.ts
@@ -0,0 +1,83 @@
+
+
+describe("[functional]", () => {
+  
+  // OAuth connects to the API and passes verification
+  // prints log statement to the terminal
+  
+  
+  // A dummy script tag is posted to the back end
+  
+  
+  // A dummy script tag is retrieved and matched
+  
+  
+  // [cleanup] the dummy script tag is deleted
+  
+  
+  // A storefront token is created and/or
+  
+  
+  
+  
+  test("1 + 2 sums correctly", () => {
+    const sum = 1 + 2;
+  
+    console.log(sum, `=====sum=====`);
+    expect(sum).toEqual(5);
+  });
+  
+  
+  test("stock sount information sends correctly", () => {
+  
+  });
+  
+  
+  
+  
+  test("", () => {
+    expect(true).toEqual(true);
+  })
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+});
